aabe9efc9adafa6c635a556f136413b9ab5acadb
macmogu <mm@moguf2k.com>
2024-08-22 00:20:58 +0800

    conform

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,18 @@
 return {
     {
         "stevearc/conform.nvim",
-        -- event = 'BufWritePre', -- uncomment for format on save
+        event = 'BufWritePre', -- uncomment for format on save
         opts = require "configs.conform",
     },
 
+    {
+        "zapling/mason-conform.nvim",
+        event = "VeryLazy",
+        dependencies = { "conform.nvim" },
+        config = function()
+            require("configs.mason-conform")
+        end,
+    },
     -- These are some examples, uncomment them if you want to see them work!
     {
         "neovim/nvim-lspconfig",

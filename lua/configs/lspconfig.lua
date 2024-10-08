c9682f0e7def0230512539c163b3aa5f77e5107b
znovid <fpack@163.com>
2024-10-08 10:02:51 +0800

    restart

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,11 +3,6 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-lspconfig.servers = {
-  -- "lua_ls",
-  "gopls",
-}
-
 -- EXAMPLE
 local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
@@ -21,67 +16,8 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- lspconfig.clangd.setup({
---     on_attach = function(client, bufnr)
---         client.server_capabilities.documentFormattingProvider = false
---         client.server_capabilities.documentRangeFormattingProvider = false
---         nvlsp.on_attach(client, bufnr)
---     end,
---     on_init = nvlsp.on_init,
---     capabilities = capabilities,
--- })
-
-lspconfig.gopls.setup({
-  on_attach = function(client, bufnr)
-    client.server_capabilities.documentFormattingProvider = false
-    client.server_capabilities.documentRangeFormattingProvider = false
-    nvlsp.on_attach(client, bufnr)
-  end,
-  on_init = nvlsp.on_init,
-  capabilities = nvlsp.capabilities,
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gotmpl", "gowork" },
-  root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
-  settings = {
-    gopls = {
-      analyses = {
-        unusedparams = true,
-      },
-      completeUnimported = true,
-      usePlaceholders = true,
-      staticcheck = true,
-    },
-  },
-})
-
--- lspconfig.lua_ls.setup({
---     on_attach = nvlsp.on_attach,
---     on_init = nvlsp.on_init,
---     capabilities = nvlsp.capabilities,
---
---     settings = {
---         Lua = {
---             diagnostics = {
---                 enable = false, -- Disable all diagnostics from lua_ls
---                 -- globals = { "vim" },
---             },
---             workspace = {
---                 library = {
---                     vim.fn.expand("$VIMRUNTIME/lua"),
---                     vim.fn.expand("$VIMRUNTIME/lua/vim/lsp"),
---                     vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types",
---                     vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy",
---                     "${3rd}/love2d/library",
---                 },
---                 maxPreload = 100000,
---                 preloadFileSize = 10000,
---             },
---         },
---     },
--- })
-
 -- configuring single server, example: typescript
--- lspconfig.tsserver.setup {
+-- lspconfig.ts_ls.setup {
 --   on_attach = nvlsp.on_attach,
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,124 +1,25 @@
 return {
-  -- {
-  --   "stevearc/conform.nvim",
-  --   -- event = 'BufWritePre', -- uncomment for format on save
-  --   opts = require "configs.conform",
-  -- },
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
 
   -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
     config = function()
-      -- require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
   },
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    event = "VeryLazy",
-    dependencies = { "nvim-lspconfig" },
-    config = function()
-      require "configs.mason-lspconfig"
-    end,
-  },
-
-  {
-    "mfussenegger/nvim-lint",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      require "configs.lint"
-    end,
-  },
-
-  {
-    "rshkarin/mason-nvim-lint",
-    event = "VeryLazy",
-    dependencies = { "nvim-lint" },
-    config = function()
-      require "configs.mason-lint"
-    end,
-  },
-
-  {
-    "stevearc/conform.nvim",
-    -- event = "BufWritePre",
-    opts = require "configs.conform",
-  },
-
-  {
-    "zapling/mason-conform.nvim",
-    event = "VeryLazy",
-    dependencies = { "conform.nvim" },
-    config = function()
-      require "configs.mason-conform"
-    end,
-  },
-
-  {
-    "mfussenegger/nvim-dap",
-    -- init = function()
-    --   require("core.util").load_mappings "dap"
-    -- end,
-  },
-  {
-    "leoluz/nvim-dap-go",
-    ft = "go",
-    dependencies = "mfussenegger/nvim-dap",
-    keys = {
-      ["<leader>dgt"] = {
-        function()
-          require("dap-go").debug_test()
-        end,
-        "Debug go test",
-      },
-      ["<leader>dgl"] = {
-        function()
-          require("dap-go").debug_last()
-        end,
-        "Debug last go test",
-      },
-    },
-    config = function(_, opts)
-      require("dap-go").setup(opts)
-    end,
-  },
-
-  {
-
-    "nvim-tree/nvim-tree.lua",
-    opts = {
-      filters = {
-        git_ignored = false,
-      },
-    },
-  },
-
-  {
-    "phaazon/hop.nvim",
-    branch = "v2", -- optional but strongly recommended
-    opts = {
-      multi_windows = true,
-      keys = "etovxqpdygfblzhckisuran",
-      upercase_labels = true,
-    },
-    keys = {
-      {
-        "<leader>fj",
-        function()
-          require("hop").hint_words()
-        end,
-        mode = { "n", "x", "o" },
-      },
-    },
-  },
-
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufWritePre", "BufNewFile" },
-    config = function()
-      require "configs.treesitter"
-    end,
-  },
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
